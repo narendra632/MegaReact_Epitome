9e17fdef10935d8eb77650e403809dc4c17dc9e6
Narendra Singh Rathore <mrperfectth7@gmail.com>
2023-11-06 11:01:05 +0530

    feat: FileUploader UI components and styling built

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,3 +1,4 @@
+import PostForm from "@/components/forms/PostForm"
 
 const CreatePost = () => {
   return (
@@ -9,6 +10,7 @@ const CreatePost = () => {
           <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
           
         </div>
+        <PostForm />
       </div>
     </div>
   )
