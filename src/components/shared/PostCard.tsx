b97bdfec2468c3ffcd1459c4901d5dfac671c344
Narendra Singh Rathore <mrperfectth7@gmail.com>
2023-11-10 23:57:14 +0530

    style: designing home post section

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
 
-const PostCard = () => {
+type PostCardProps = {
+    post: Models.Document;
+}
+
+const PostCard = ({ post }: PostCardProps) => {
   return (
-    <div>PostCard</div>
+    <div className="post-card">
+        <div className="flex-between">
+            <div className="flex items-center gap-3">
+                <Link to={`/profile/${post.creator.$id}`}>
+                    <img src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'} className="rounded-full w-12 lg:h-12" alt="creator" /> 
+                </Link>
+                <div className="flex flex-col">
+                    <p className="base-medium lg:body-bold text-light-1">{post.creator.name}</p>
+                    <div>
+                        <p>{post.$createdAt}</p>- <p>{post.location}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
